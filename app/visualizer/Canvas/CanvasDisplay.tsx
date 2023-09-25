3d3cccb80586d68a2ee9df2842db2f03ca03925b
RobPruzan <rapruzan@buffalo.edu>
2023-09-24 21:47:57 -0400

    renable sockets in prod

--- app/visualizer/Canvas/CanvasDisplay.tsx
+++ app/visualizer/Canvas/CanvasDisplay.tsx
@@ -152,10 +152,7 @@ const CanvasDisplay = ({
   const cursorImgRef = useRef<HTMLImageElement | null>(null);
   useEffect(() => {
     // putting a condition here of session.status === 'loading' still breaks even with the updated cleanup logic
-    if (process.env.NODE_ENV === 'production') {
-      // temporarily disable in prod
-      return;
-    }
+
     if (!playgroundID) return;
 
     dispatch({
