ce95179c5bd8825b35f3c40070afb8af0101e7af
RobPruzan <rapruzan@buffalo.edu>
2023-10-02 03:32:14 -0400

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
             </div>
             <div className="border-2 shadow-2 shadow-accent shadow-md rounded-md p-3 hidden  w-64 sm:w-72 lg:w-[16.5rem] h-32 sm:h-40 sm:flex flex-col">
               <div className="h-full w-full  text-2xl flex justify-center items-center text-foreground font-bold">
-                {'Backtracing'}
+                {'Backtracking'}
               </div>
               <div className="h-full w-full  flex items-center justify-center ">
                 <Link
